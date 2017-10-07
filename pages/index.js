e0a078c9a6d16989c7d1fe71f150126f6f363905
Tim Neutkens <timneutkens@me.com>
2017-10-07 09:01:06 +0200

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,42 @@
+export default () => <svg
+    width="153px"
+    height="72px"
+    viewBox="0 0 153 72"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+  >
+   
+    <defs>
+      <linearGradient
+        x1="15.304102%"
+        y1="0%"
+        x2="58.4710885%"
+        y2="60.4158151%"
+        id="linearGradient-1"
+      >
+        <stop stopColor="#000" offset="0%" />
+        <stop stopColor="#111" offset="100%" />
+      </linearGradient>
+    </defs>
+    <g
+      stroke="none"
+      strokeWidth={1}
+      fill="none"
+      fillRule="evenodd"
+    >
+      <g
+        transform="translate(-334.000000, -6900.000000)"
+      >
+        <g transform="translate(334.000000, 6900.000000)">
+          <g>
+            <polygon
+              fill="url(#linearGradient-1)"
+              transform="translate(80.574575, 39.531335) translate(-87.574575, -39.531335) "
+              points="2.84726847 7.6672614 7.9818554 61.3954094 123.16515 61.3954094 15.301881 1.6672614"
+            />
+          </g>
+        </g>
+      </g>
+    </g>
+  </svg>
---
+++ readme.md
@@ -0,0 +1,8 @@
+Error:
+> Warning: Expected server HTML to contain a matching <linearGradient> in <defs>.
+
+To run the example:
+
+`yarn`
+
+`yarn run dev`
